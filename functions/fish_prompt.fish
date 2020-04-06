36865b85dbac7335a2d1ba2e39afc931617fb556
Dang <dang@travelstop.com>
2020-04-07 02:02:35 +0800

    fix starship does not reflect universal AWS_ENV across instances

---
+++ functions/fish_prompt.fish
@@ -0,0 +1,10 @@
+function fish_prompt --description 'wrap starship prompt to fix environment variables not refreshed'
+  if not functions -q starship_fish_prompt
+    # rename fish_prompt by starship
+    source (starship init fish --print-full-init | string replace fish_prompt starship_fish_prompt | psub)
+  end
+
+  set -gx AWS_ENV $AWS_ENV
+  starship_fish_prompt
+  set -e AWS_ENV
+end
