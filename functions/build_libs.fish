1e0fe4c82259b640c0927fb5c9a8e584959ed171
Dang <dang@travelstop.com>
2020-04-13 20:33:47 +0800

    fix build_libs doesn't regenerate package-lock.json

--- functions/build_libs.fish
+++ functions/build_libs.fish
@@ -19,5 +19,5 @@ function build_libs --description "rebuild libs module"
 
   # rebuild package-lock and reinstall libs
   echo (set_color --background green)(set_color black)reinstall packages(set_color normal)
-  npm install --prefix "$nodejs_dir" --production --no-shrinkwrap
+  npm install --prefix "$nodejs_dir" --production --no-shrinkwrap --package-lock-only
 end
