3ee805f18440d274dd580deae0fe610e8005b43a
Dang <dang@travelstop.com>
2020-04-20 05:25:05 +0800

    watch logs right after invoke

--- functions/invoke.fish
+++ functions/invoke.fish
@@ -1,5 +1,6 @@
-function sls_invoke --description 'invoke lambda function'
+function sls_invoke --argument-names function_name --description 'invoke lambda function'
   sls invoke --stage $AWS_PROFILE --type Event --function $argv
+  sls_logs $function_name
 end
 
 alias invoke=sls_invoke
