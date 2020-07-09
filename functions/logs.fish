9e2e9cfdf2df4d8621bc963e8298ac4f2ec96aae
Dang Huynh <huynhminhdang@gmail.com>
2020-07-10 00:23:31 +0800

    use awk to transform log

--- functions/logs.fish
+++ functions/logs.fish
@@ -6,7 +6,32 @@ function logs --argument-names function_name start_time --description "watch lam
   set --local command "sls logs --aws-profile $AWS_PROFILE --stage $stage --tail --startTime $start_time --function $function_name"
   echo (set_color green)$command(set_color normal)
   # eval "$command | __logs_transform"
-  eval $command
+  set --local logs_and_transform $command '| awk \'{
+    gsub(/^[0-9:. ()+-]{32}[[:space:]]+[a-z0-9-]{36}[[:space:]]+INFO[[:space:]]+/, "")
+    if ($0 ~ /^\[([A-Z-]+)\]\[([0-9TZ:.-]{24})\]\[([a-z.-]+):([0-9]+)\]\[[a-z.]+\]/) {
+      match($0, /^\[([A-Z-]+)\]\[([0-9TZ:.-]{24})\]\[([a-z.-]+):([0-9]+)\]\[[a-z.]+\]/)
+      rest = substr($0, RLENGTH+1)
+      split($0, tokens, /[\[\]]/)
+      stage = tokens[2]
+      time = tokens[4]
+      split(tokens[6], location, /:/)
+      filename = location[1]
+      lineno = location[2]
+      method = tokens[8]
+      $0 = "\x1b[90m[\x1b[34m"stage"\x1b[90m][\x1b[36m"time"\x1b[90m][\x1b[35m"filename"\x1b[90m:\x1b[35m"lineno"\x1b[90m][\x1b[34m"method"\x1b[90m]\x1b[0m"rest
+    }
+    if ($0 ~ /^(START|END|REPORT) RequestId.*/) {
+      $0 = "\x1b[90m"$0"\x1b[0m"
+    } else if ($0 ~ /^XRAY TraceId.*/) {
+      $0 = "\x1b[90m"$0"\x1b[0m"
+    }
+    gsub(/\[INFO\]:/, "[INFO]:\x1b[32m")
+    gsub(/\[WARN\]:/, "[WARN]:\x1b[33m")
+    gsub(/\[ERROR\]:/, "[ERROR]:\x1b[31m")
+    gsub(/\[DEBUG\]:/, "[DEBUG]:\x1b[90m")
+    print
+  }\''
+  eval $logs_and_transform
 end
 
 function __logs_transform
