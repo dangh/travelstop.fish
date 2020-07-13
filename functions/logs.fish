46a9f218e1d3a9b294390299dc53f37878cbd826
Dang Huynh <huynhminhdang@gmail.com>
2020-07-13 18:53:53 +0800

    clean

--- functions/logs.fish
+++ functions/logs.fish
@@ -6,49 +6,63 @@ function logs --argument-names function_name start_time --description "watch lam
   set --local command "sls logs --aws-profile $AWS_PROFILE --stage $stage --tail --startTime $start_time --function $function_name"
   echo (set_color green)$command(set_color normal)
   # eval "$command | __logs_transform"
-  set --local logs_and_transform $command '| awk \'{
-    #remove aws timestamp, log id, log level
-    gsub(/^[0-9:. ()+-]{32}[[:space:]]+[a-z0-9-]{36}[[:space:]]+INFO[[:space:]]+/, "")
+  set --local command "cat log"
+  set --local logs_and_transform $command '| awk \'
+    function resetAfter(s) { return sprintf("%s\x1b[0m", s) }
+    function bold(s) { return sprintf("\x1b[1m%s", s) }
+    function black(s) { return sprintf("\x1b[30m%s", s) }
+    function red(s) { return sprintf("\x1b[31m%s", s) }
+    function green(s) { return sprintf("\x1b[32m%s", s) }
+    function yellow(s) { return sprintf("\x1b[33m%s", s) }
+    function blue(s) { return sprintf("\x1b[34m%s", s) }
+    function magenta(s) { return sprintf("\x1b[35m%s", s) }
+    function cyan(s) { return sprintf("\x1b[36m%s", s) }
+    function brightBlack(s) { return sprintf("\x1b[90m%s", s) }
+    {
+      #remove aws timestamp, log id, log level
+      gsub(/^[0-9:. ()+-]{32}[[:space:]]+[a-z0-9-]{36}[[:space:]]+INFO[[:space:]]+/, "")
 
-    #blue stage
-    #cyan timestamp
-    #magenta source location
-    #blue method name
-    if ($0 ~ /^\[([A-Z-]+)\]\[([0-9TZ:.-]{24})\]\[([a-z.-]+):([0-9]+)\]\[[a-zA-Z.]+\]/) {
-      match($0, /^\[([A-Z-]+)\]\[([0-9TZ:.-]{24})\]\[([a-z.-]+):([0-9]+)\]\[[a-zA-Z.]+\]/)
-      rest = substr($0, RLENGTH+1)
-      split($0, tokens, /[\[\]]/)
-      stage = tokens[2]
-      time = tokens[4]
-      split(tokens[6], location, /:/)
-      filename = location[1]
-      lineno = location[2]
-      method = tokens[8]
-      $0 = "\x1b[90m[\x1b[34m"stage"\x1b[90m][\x1b[36m"time"\x1b[90m][\x1b[35m"filename"\x1b[90m:\x1b[35m"lineno"\x1b[90m][\x1b[34m"method"\x1b[90m]\x1b[0m"rest
+      #blue stage
+      #cyan timestamp
+      #magenta source location
+      #blue method name
+      if ($0 ~ /^\[([A-Z-]+)\]\[([0-9TZ:.-]{24})\]\[([a-z.-]+):([0-9]+)\]\[[a-zA-Z.]+\]/) {
+        match($0, /^\[([A-Z-]+)\]\[([0-9TZ:.-]{24})\]\[([a-z.-]+):([0-9]+)\]\[[a-zA-Z.]+\]/)
+        rest = substr($0, RLENGTH+1)
+        split($0, tokens, /[\[\]]/)
+        stage = tokens[2]
+        time = tokens[4]
+        split(tokens[6], location, /:/)
+        filename = location[1]
+        lineno = location[2]
+        method = tokens[8]
+        $0 = brightBlack("[") blue(stage) brightBlack("]") brightBlack("[") cyan(time) brightBlack("]") brightBlack("[") magenta(filename) brightBlack(":") magenta(lineno) brightBlack("]") brightBlack("[") blue(method) brightBlack("]") rest
 
-      #blank line before each log entry
-      printf "\n"
-    }
+        #blank line before each log entry
+        printf "\n"
+      }
 
-    #blank line after last log entry
-    if ($0 ~ /^END RequestId/) printf "\n"
+      #blank line after last log entry
+      if ($0 ~ /^END RequestId/) printf "\n"
 
-    #blank page before each event
-    if ($0 ~ /^START RequestId/) printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+      #blank page before each event
+      if ($0 ~ /^START RequestId/) printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 
-    #bright black aws logs
-    gsub(/^(START|END|REPORT|XRAY) (RequestId|TraceId).*/, "\x1b[90m"$0"\x1b[0m")
+      #bright black aws logs
+      gsub(/^(START|END|REPORT|XRAY) (RequestId|TraceId).*/, brightBlack($0))
 
-    #green info
-    #yellow warn
-    #red error
-    #bright black debug
-    gsub(/\[INFO\]:/, "[INFO]:\x1b[32m")
-    gsub(/\[WARN\]:/, "[WARN]:\x1b[33m")
-    gsub(/\[ERROR\]:/, "[ERROR]:\x1b[31m")
-    gsub(/\[DEBUG\]:/, "[DEBUG]:\x1b[90m")
-    print
-  }\''
+      #green info
+      #yellow warn
+      #red error
+      #bright black debug
+      gsub(/\[INFO\]: /, sprintf("%s%s%s%s", brightBlack("["), resetAfter(bold(green("INFO"))), resetAfter(brightBlack("]\n")), green()))
+      gsub(/\[WARN\]: /, sprintf("%s%s%s%s", brightBlack("["), resetAfter(bold(yellow("WARN"))), resetAfter(brightBlack("]\n")), yellow()))
+      gsub(/\[ERROR\]: /, sprintf("%s%s%s%s", brightBlack("["), resetAfter(bold(red("ERROR"))), resetAfter(brightBlack("]\n")), red()))
+      gsub(/\[DEBUG\]: /, sprintf("%s%s%s%s", brightBlack("["), resetAfter(bold(black("DEBUG"))), resetAfter(brightBlack("]\n")), black()))
+
+      print
+    }\'
+  '
   eval $logs_and_transform
 end
 
