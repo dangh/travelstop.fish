24269864c71a48f6f7f8981382ced17a50f9ea27
Dang Huynh <huynhminhdang@gmail.com>
2020-07-28 22:58:31 +0800

    allow invoke with sls parameters

--- functions/invoke.fish
+++ functions/invoke.fish
@@ -1,7 +1,10 @@
 function invoke --argument-names function_name --description "invoke lambda function"
   set --local stage (string lower -- (string replace --regex ".*@" "" -- $AWS_PROFILE))
   set --local start_time (date -u "+%Y%m%dT%H%M%S")
-  set --local command "sls invoke --aws-profile $AWS_PROFILE --stage $stage --type Event --function $argv"
+  set --local command "sls invoke --aws-profile $AWS_PROFILE --stage $stage --type Event --function $function_name"
+  for i in $argv[2..-1]
+    set command $command "'$i'"
+  end
   echo (set_color green)$command(set_color normal)
   eval $command
   logs $function_name $start_time
