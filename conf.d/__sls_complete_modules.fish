1ca67ac194e24cea29aff3a03f26da14e94f724d
Dang <dang@travelstop.com>
2020-12-07 04:16:35 +0800

    fix error when install from non-project dir

--- conf.d/__sls_complete_modules.fish
+++ conf.d/__sls_complete_modules.fish
@@ -1,4 +1,4 @@
 function __sls_complete_modules
-  set --local project_dir (git rev-parse --show-toplevel)
-  ls "$project_dir/modules"
+  set --local project_dir (git rev-parse --show-toplevel 2>/dev/null)
+  test $status -eq 0 && ls "$project_dir/modules"
 end
