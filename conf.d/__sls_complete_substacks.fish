969f8cc56bd8a59cb20822dbe193e9f8f521b8d2
Dang <dang@travelstop.com>
2020-12-07 04:24:31 +0800

    only find substacks in direct child directories
    Closes #1

--- conf.d/__sls_complete_substacks.fish
+++ conf.d/__sls_complete_substacks.fish
@@ -1,3 +1,3 @@
 function __sls_complete_substacks
-  find ./*/ -name serverless.yml | awk '{ gsub("/serverless.yml", ""); gsub("^./", ""); print }'
+  find . -name serverless.yml -maxdepth 2 -mindepth 2 | awk '{ gsub("/serverless.yml", ""); gsub("^./", ""); print }'
 end
