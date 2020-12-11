a92625a0569bb41268be9f04e0bf1d8cdd7c88a8
Dang <dang@travelstop.com>
2020-12-11 20:31:16 +0800

    fix push completion

--- completions/push.fish
+++ completions/push.fish
@@ -1,14 +1,6 @@
-for name in (__sls_complete_modules)
-  complete --keep-order --no-files --command push --condition "not __fish_seen_subcommand_from $name" --arguments $name --description "module"
-end
-
-for name in (__sls_complete_functions)
-  complete --keep-order --no-files --command push --condition "not __fish_seen_subcommand_from $name" --arguments $name --description "function"
-end
-
-for name in (__sls_complete_substacks)
-  complete --keep-order --no-files --command push --condition "not __fish_seen_subcommand_from $name" --arguments $name --description "sub-stack"
-end
+complete --keep-order --no-files --command push --arguments "(__sls_complete_modules)" --description "module"
+complete --keep-order --no-files --command push --arguments "(__sls_complete_functions)" --description "function"
+complete --keep-order --no-files --command push --arguments "(__sls_complete_substacks)" --description "sub-stack"
 
 # enforce no-files when all completions are selected
 complete --no-files --command push
