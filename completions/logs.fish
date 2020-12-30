8ae8fdf720bb7e1fa6f31a8851c3294133f99d58
Dang <dang@travelstop.com>
2020-12-31 03:06:59 +0800

    unique completion

--- completions/logs.fish
+++ completions/logs.fish
@@ -1,2 +1,2 @@
-complete --no-files --command logs --condition "not __fish_seen_subcommand_from (__sls_complete_functions)" --arguments "(__sls_complete_functions)" --description "function"
+complete --no-files --command logs --arguments "(__sls_complete_uniq (__sls_complete_functions))" --description "function"
 complete --no-files --command logs
