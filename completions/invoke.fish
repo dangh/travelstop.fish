afd60fe07c357c32c5d05dc725a6177e113235ab
Dang <dang@travelstop.com>
2021-01-26 19:22:36 +0800

    clean up completion

--- completions/invoke.fish
+++ completions/invoke.fish
@@ -1,2 +1,2 @@
-complete --no-files --command invoke --condition "not __fish_seen_subcommand_from (_ts_functions)" --arguments "(_ts_uniq_completions (_ts_functions))" --description "function"
+complete --command invoke --condition "not __fish_seen_subcommand_from (_ts_functions)" --arguments "(_ts_uniq_completions (_ts_functions))" --description "function"
 complete --no-files --command invoke
--- completions/push.fish
+++ completions/push.fish
@@ -1,6 +1,6 @@
-complete --keep-order --no-files --command push --arguments "(_ts_uniq_completions (_ts_modules))" --description "module"
-complete --keep-order --no-files --command push --arguments "(_ts_uniq_completions (_ts_functions))" --description "function"
-complete --keep-order --no-files --command push --arguments "(_ts_uniq_completions (_ts_substacks))" --description "sub-stack"
+complete --command push --arguments "(_ts_uniq_completions (_ts_modules))" --description "module"
+complete --command push --arguments "(_ts_uniq_completions (_ts_functions))" --description "function"
+complete --command push --arguments "(_ts_uniq_completions (_ts_substacks))" --description "sub-stack"
 
 # enforce no-files when all completions are selected
 complete --no-files --command push
