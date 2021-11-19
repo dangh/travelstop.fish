505912230566025f6947a49d46a83074b4e9fdf1
Dang <huynhminhdang@gmail.com>
2021-11-19 12:22:54 +0800

    add changes function

---
+++ functions/changes.fish
@@ -0,0 +1,10 @@
+function changes --argument-names from --description "print list of changed services and modules"
+  test -n "$from" || set --local from HEAD~1
+  set --local jsons (git diff --name-only --line-prefix=(git rev-parse --show-toplevel)/ $from | sed -E 's/[^/]+$/package.json/' | uniq | grep -v /lib/ | grep -v /schema/)
+  for json in $jsons
+    test -f $json || continue
+    set --local name (echo (string match --regex '"name": "([^"]+)"' < $json)[2] | sed 's/travelstop-//')
+    set --local v (string match --regex '"version": "([^"]+)"' < $json)[2]
+    echo $name-$v
+  end
+end
