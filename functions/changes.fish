0af6d852f84aa1dc1e0c4fb0d5572c1926f32338
Dang <huynhminhdang@gmail.com>
2021-12-15 15:44:06 +0800

    changes: compare to master by default

--- functions/changes.fish
+++ functions/changes.fish
@@ -1,5 +1,5 @@
 function changes --argument-names from --description "print list of changed services and modules"
-  test -n "$from" || set --local from HEAD~1
+  test -n "$from" || set --local from (git merge-base master HEAD)
   set --local jsons (git diff --name-only --line-prefix=(git rev-parse --show-toplevel)/ $from | sed -E 's/[^/]+$/package.json/' | sort | uniq | grep -v /lib/ | grep -v /schema/)
   for json in $jsons
     test -f $json || continue
