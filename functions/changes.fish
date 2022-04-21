f5c37a1aa6a28656eb43ba5c367f8a1123584f1b
Dang <huynhminhdang@gmail.com>
2022-04-21 18:34:38 +0700

    changes: diff HEAD again master by default, includes all packages

--- functions/changes.fish
+++ functions/changes.fish
@@ -1,19 +1,24 @@
 function changes --argument-names from --description "print list of changed services and modules"
-  test -n "$from" || set --local from (git merge-base master HEAD)
-  set --local changes (git diff --name-only --line-prefix=(git rev-parse --show-toplevel)/ $from | string split0)
-  set --local jsons (echo $changes | sed -E 's/[^/]+$/package.json/' | grep -v '/lib/' | grep -v '/schema/' | sort | uniq)
-  set --local ymls (echo $changes | grep '.yml' | grep -v 'modules/' | grep -v 'serverless-layers' | sed -E 's/[^/]+$/serverless.yml/' | sort | uniq)
+  argparse --ignore-unknown 'i/index' -- $argv
+  if set --query _flag_index
+    set --function range (git merge-base origin/master HEAD)
+  else if test -z "$from"
+    set --function range origin/master...
+  else
+    set --function range $from...
+  end
+  set --local package_jsons
+  set --local serverless_ymls
   set --local stack_names
   set --local stack_versions
-  for yml in $ymls
-    test -f $yml || continue
-    string match --quiet --regex '^service:\s*(?<name>\S+)\s*$' < $yml
-    if not contains -- $name $stack_names
-      set --append stack_names $name
-      set --append stack_versions ''
+  set --local root (git rev-parse --show-toplevel)
+  for path in (git diff --name-only $range | grep -E '^(modules|services|web)/')
+    while set path (string replace --regex '/[^/]+$' '' $path)
+      contains $root/$path/package.json $package_jsons || set --append package_jsons $root/$path/package.json
+      contains $root/$path/serverless.yml $serverless_ymls || set --append serverless_ymls $root/$path/serverless.yml
     end
   end
-  for json in $jsons
+  for json in $package_jsons
     test -f $json || continue
     string match --quiet --regex '"name": "(travelstop-)?(?<name>[^"]+)"' < $json
     string match --quiet --regex '"version": "(?<v>[^"]+)"' < $json
@@ -24,7 +29,16 @@ function changes --argument-names from --description "print list of changed serv
       set --append stack_versions -$v
     end
   end
-  for index in (seq (count $stack_names))
-    echo $stack_names[$index]$stack_versions[$index]
+  for yml in $serverless_ymls
+    test -f $yml || continue
+    string match --quiet --regex '^service:\s*(?<name>module-\w+|\S+)\S*\s*$' < $yml
+    if not contains -- $name $stack_names
+      set --append stack_names $name
+      set --append stack_versions ''
+    end
+  end
+  for name in $stack_names
+    set --local v $stack_versions[(contains --index -- $name $stack_names)]
+    echo $name$v
   end | sort
 end
