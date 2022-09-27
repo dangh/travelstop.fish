236a60b627363d14586728a8f360d9cb3fc03efb
Dang <huynhminhdang@gmail.com>
2022-09-27 11:13:30 +0700

    rename_modules: rename changed modules only (#9)
    Closes #9

--- functions/rename_modules.fish
+++ functions/rename_modules.fish
@@ -2,18 +2,41 @@ function rename_modules -a suffix
   # ensure we're inside workspace
   test -d $$_ts_project_dir || exit 1
 
-  # given no suffix, and there's no suffix currently,
-  # use current git branch as suffix
-  if test -z "$suffix" && string match -q -r 'module-libs$' -- (cat $$_ts_project_dir/modules/libs/serverless.yml)
-    set -l branch (git branch --show-current)
-    if test "$branch" != 'master'
-      set suffix (string replace -a -r '\W+' '-' -- $branch)
+  set -l modules
+
+  # find changed modules
+  git diff --name-only master | while read -l -L file
+    switch $file
+      case modules/libs\* lib/\* schema/\*
+        contains libs $modules || set -a modules libs
+      case modules/chrome/\*
+        contains chrome $modules || set -a modules chrome
+      case modules/ghostscript/\*
+        contains ghostscript $modules || set -a modules ghostscript
+      case modules/sharp/\*
+        contains sharp $modules || set -a modules sharp
+      case modules/templates/\*
+        contains templates $modules || set -a modules templates
     end
   end
 
-  test -n "$suffix" && set suffix "-$suffix"
+  if test -z "$suffix"
+    # if any module already has suffix
+    if not string match -q -r 'module-('(string join '|' $modules)')-\$' -- < $$_ts_project_dir/services/serverless-layers.yml
+      # toggle off suffix
+      set suffix ''
+    else
+      # use git branch as suffix
+      set -l branch (git branch --show-current)
+      if test "$branch" != 'master'
+        set suffix (string replace -a -r '\W+' '-' -- $branch)
+      end
+    end
+  end
 
-  sed -i '' -E 's/module-(libs|templates)([^$]*)(-\$.*)?$/module-\1'"$suffix"'\3/g' \
+  # rename modules
+  test -n "$suffix" && set suffix "-$suffix"
+  sed -i '' -E 's/module-('(string join '|' $modules)')([^$]*)(-\$.*)?$/module-\1'"$suffix"'\3/g' \
     $$_ts_project_dir/modules/libs/serverless.yml \
     $$_ts_project_dir/modules/templates/serverless.yml \
     $$_ts_project_dir/services/serverless-layers.yml \
