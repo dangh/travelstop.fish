ab2c4939eb169dc24205a572a3acbf3bed82ff09
Dang <huynhminhdang@gmail.com>
2024-04-06 19:15:59 +0800

    sls: only use assign profile stage region when needed

--- functions/sls.fish
+++ functions/sls.fish
@@ -4,7 +4,9 @@ function sls -d "wraps sls to provide stage/profile/region implicitly"
     set -l region $AWS_DEFAULT_REGION
     set -l data
 
-    argparse -i \
+    set -l args $argv
+
+    argparse -i -s \
         'profile=' \
         's/stage=' \
         'r/region=' \
@@ -13,18 +15,24 @@ function sls -d "wraps sls to provide stage/profile/region implicitly"
         -- $argv
     or return 1
 
-    set -q _flag_profile && set profile $_flag_profile
-    set -q _flag_stage && set stage $_flag_stage
-    set -q _flag_region && set region $_flag_region
+    set -l cmd
+    set -l sub_command $argv[1]
+    if test -n "$sub_command"
+        set cmd sls $args
+    else
+        set -q _flag_profile && set profile $_flag_profile
+        set -q _flag_stage && set stage $_flag_stage
+        set -q _flag_region && set region $_flag_region
 
-    set -l yml serverless.yml
-    set -q _flag_config && set yml $_flag_config
-    string match -q -r '^\s*region:\s*\'(?<yml_region>[a-z0-9-]+)\'' <$yml
-    test -n "$yml_region" && set region $yml_region
+        set -l yml serverless.yml
+        set -q _flag_config && set yml $_flag_config
+        string match -q -r '^\s*region:\s*\'(?<yml_region>[a-z0-9-]+)\'' <$yml
+        test -n "$yml_region" && set region $yml_region
 
-    set -l cmd sls $argv --profile=$profile --stage=$stage --region=$region
-    test -n "$_flag_data" && set -a cmd --data=(string replace -r -a '\s*\n\s*' ' ' -- $_flag_data | string collect | string escape)
+        set -l cmd sls $argv --profile=$profile --stage=$stage --region=$region
+        test -n "$_flag_data" && set -a cmd --data=(string replace -r -a '\s*\n\s*' ' ' -- $_flag_data | string collect | string escape)
 
-    _ts_log execute command: (green (string join ' ' -- (_ts_env --mode=env) $cmd))
+        _ts_log execute command: (green (string join ' ' -- (_ts_env --mode=env) $cmd))
+    end
     eval (_ts_env --mode=env) command $cmd
 end
