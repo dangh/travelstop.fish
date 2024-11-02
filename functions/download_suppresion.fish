bbcac70269f8633fd81472ac3330ee7dfc228a15
Dang <huynhminhdang@gmail.com>
2024-11-02 16:26:38 +0800

    add download_suppresion

---
+++ functions/download_suppresion.fish
@@ -0,0 +1,19 @@
+function download_suppression
+    set files
+    set page 0
+    set token
+    while true
+        set page (math $page + 1)
+        set file (mktemp)
+        set -a files $file
+        set -l args
+        test -n "$token" && set -a args --next-token $token
+        aws sesv2 list-suppressed-destinations $args >$file
+        set token (jq -r '.NextToken' $file)
+        if test "$token" = null
+            break
+        end
+    end
+    jq -s '{ SuppressedDestinationSummaries: .[].SuppressedDestinationSummaries | sort_by(.LastUpdateTime) | reverse }' $files
+    rm -f $files
+end
